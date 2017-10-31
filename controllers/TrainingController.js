33bf97ea30ce36e7c52b2030968d2aeee181ceb4
Javier Escolar Salcedo <javierescolar10@gmail.com>
2017-10-31 18:29:01 +0100

    solucion de error al filtrar por el objectId

--- controllers/TrainingController.js
+++ controllers/TrainingController.js
@@ -2,14 +2,11 @@ var Training = require("../models/Training").Training;
 
 
 exports.index = function(req,res) {
-  Training.find({})
-    .populate("type_training_id")
-    .exec(function(err, training) {
+  Training.find({type_training_id: req.params.id},(err, trainings) => {
       if (err) {
         res.status(500).send(err);
       } else {
-        res.status(200).send(training);
+        res.status(200).send(trainings);
       }
     });
-  
 }
